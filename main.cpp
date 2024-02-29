95588620c7249214ba24c8b41a6c84873e27827a
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-02-29 10:19:31 +0100

    aaa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <list>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
@@ -40,15 +43,58 @@ private:
     int jersey;
 };
 
+         //1000  1004           1008            1012           1016                      1020
+// array   1,     3,           333             453543,        543534,                  543543
+// insert pos 2 , 333          
+
+
+// LIST    - implementera std::list 
+// - implementera std::queue
+// - implementera std::stack
 
 int main(){
 
-    std::vector<Player> greatPlayers{
+    std::queue<std::string>  printerQueue;
+
+    printerQueue.push("https://blabla.se");
+    printerQueue.push("https://blabla2.se");
+    printerQueue.push("https://blabla3.se");
+
+    while(printerQueue.size()){
+        std::string s = printerQueue.front(); // PEEK
+        //print(s)
+         printerQueue.pop();
+    }
+    
+
+
+
+    // std::stack<std::string>  history;
+
+    // history.push("https://blabla.se");
+    // history.push("https://blabla2.se");
+    // history.push("https://blabla3.se");
+    
+    // history.pop(); // back!   "https://blabla3.se" försvinner
+    // std::string newUrl =  history.top(); // https://blabla2.se
+    
+
+
+
+
+    std::list<Player> greatPlayers{
         Player("Peter Forsberg",1973,Player::PlayerType::PlayerType_Forward,21),
         Player("Mats Sundin",1971,Player::PlayerType::PlayerType_Forward,13), 
-        Player("Henrik Zetterg",1980,Player::PlayerType::PlayerType_Forward,40)
+        Player("Henrik Zetterberg",1980,Player::PlayerType::PlayerType_Forward,40)
     };
     Player oneMore = Player("Peter Lindmark",1956,Player::PlayerType::PlayerType_Goalie,1);
+    
+    //Player &p = greatPlayers.
+    //greatPlayers.insert(12, oneMore);
+    //greatPlayers.insert(std::begin(greatPlayers), oneMore);
+    //greatPlayers.push_back(oneMore);
+    //greatPlayers.insert()
+
     // 
     
 
